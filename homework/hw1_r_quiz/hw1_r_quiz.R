83549e0b8ce18bbc4cf45e07dcd24197d952d4b8
Anna Nguyen <anna-nguyen@berkeley.edu>
2019-08-12 15:58:32 -0700

    add assessment quiz

---
+++ homework/hw1_r_quiz/hw1_r_quiz.R
@@ -0,0 +1,157 @@
+######################################################
+# Welcome to Public Health 250F! 
+# In this course, you'll learn how to use R to complete 
+# data analysis in epidemiology. In your first assignment, 
+# you'll learn the basics of the language. 
+
+# You can receive credit for this assignment by completing 
+# one of the following:
+#   (1) Complete the datacamp tutorials for both R and 
+#       tidyverse and upload a certifcate of completion 
+#       on bCourses.
+
+#   (2) Completing this quiz
+
+# For each question below, replace 
+# "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+# with your answer. For example, if your answer to Question 1
+# is (a), replace q_1 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+# with q_1 = "a"
+######################################################
+
+source('setup/autograder-setup/hw1_r_quiz/hw1_r_quiz.ok.R')
+AutograderInit()
+
+######################################################
+# *Question 1*
+# How do we load the "dplyr" package into our R programming environment? 
+# Assign `q_1` to the character corresponding your answer choice.
+#  a.	load(dplyr)
+#  b.	launch(dplyr)
+#  c.	package(dplyr)
+#  d.	library(dplyr)
+######################################################
+
+q_1 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem1()
+
+######################################################
+# *Question 2*
+# How do we load the dataset "my_data.csv" into our environment? 
+# Assign `q_2` to the character corresponding your answer choice.
+#  a. load(my_data.csv)
+#  b. read.csv("my_data.csv")
+#  c. launch(my_data.csv)
+#  d. load("my_data.csv")
+######################################################
+  
+q_2 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem2()
+
+######################################################
+# *Question 3*
+# How do we load the dataset "my_data.RData" into our environment? 
+# Assign `q_3` to the character corresponding your answer choice.
+#  a. load(my_data.RData)
+#  b. launch(my_data.RData)
+#  c. load("my_data.RData")
+#  d. read.csv("my_data.RData")
+######################################################
+
+q_3 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem3()
+
+######################################################
+# *Question 4*
+# Assign `q_4` to a vector with the following numerics: 
+# 2, 2 times 5, 3 divided by 4, and 4 to the 5th power. 
+######################################################
+
+q_4 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem4()
+
+######################################################
+# *Question 5*
+# Assign `q_5` to a vector containing the second, third, 
+# and fourth item of the object list_of_numbers, without using c().
+######################################################
+list_of_numbers = c(0, 2, 10, 12, 4, 43, 6)
+q_5 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>" 
+CheckProblem5()
+
+######################################################
+# *Question *
+# Assign the column names of a data frame called df 
+# to q_6.
+######################################################
+
+df = readRDS("df.RDS")
+q_6 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem6()
+
+######################################################
+# For the following question, assume that we have the 
+# table `q_7`, describing a store's inventory with the columns:
+#  - `item_name`: the name of the item
+#  - `amount_remaining`: number of items remaining
+#  - `price_per_item`: cost of one item
+# Using base R methods, add a column to `q_7` called 
+# `total_cost`, which contains the total price of all 
+# the remaining items for each `item_name`.
+######################################################
+
+q_7 = readRDS("q_7.RDS")
+q_7 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>" 
+CheckProblem7()
+
+######################################################
+# *Question 8*
+# What function can we use to add columns to our data frame? 
+# Assign `q_8` to the character corresponding your answer choice.
+#  a. mutate(...)
+#  b. add_col(...)
+#  c. summarize(...)
+#  d. combine(...)
+######################################################
+
+q_8 = "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem8()
+
+######################################################
+# For the following questions, use the data table 
+# `student_info`, which contains the columns:
+#  - `studentID`: integers with student ID numbers
+#  - `name`: strings with student names
+#  - `major`: strings of student majors. Assume that 
+# students only have one major
+#  - `age`: integers with student ages
+#  - `terms`: integers with the number of semesters 
+# students have been in attendance
+#  - `gpa`: integers with student GPAs
+######################################################
+
+student_info = readRDS("student_info.RDS")
+
+######################################################
+# *Question 9*
+# Assign `q_6` to a data table containing rows from 
+# `student_info` for students who are Public Health majors.
+######################################################
+
+q_9 = student_info %>% "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem9()
+
+######################################################
+# *Question 10*
+# Assign `q_10` to a copy of `student_info` sorted on 
+# age in descending order.
+######################################################
+
+q_10 = student_info %>% "<<<<<<<<<<<<< YOUR CODE HERE >>>>>>>>>>>>>>>"
+CheckProblem10()
+
+# --------------------------------------------
+# Check your total score
+MyTotalScore()
+# --------------------------------------------
+
---
+++ setup/autograder-setup/hw1_r_quiz/hw1_r_quiz.ok.R
@@ -0,0 +1,177 @@
+#################################################
+# R-for-Epi
+# Epidemiologic Methods II (PHW250F, PHW250G)
+# created by Jade Benjamin-Chung
+
+# Autograder: R-Quiz
+###############################################
+
+source("setup/autograder-setup/autograder_setup.R")
+
+AutograderInit = function() {
+  AutograderSetUp(10)   # Put total number of questions here
+}
+
+CheckProblem1 = function() {
+  problemNumber <<- 1
+  scores[problemNumber] <<- 0
+  num_tests <<- 3
+  tests_failed <<- num_tests
+
+  TestCase((class(q_1) == "character" & length(q_1) == 1), error_message = "Make sure that your answer is a character!")
+  TestCase((q_1 %in% c("a", "b", "c", "d")), error_message = "Make sure that your answer is one of the options listed!")
+  TestCase((q_1 == "d"), error_message = "Incorrect choice, try again!")
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem2 = function() {
+  problemNumber <<- 2
+  scores[problemNumber] <<- 0
+  num_tests <<- 3
+  tests_failed <<- num_tests
+  
+  TestCase((class(q_2) == "character" & length(q_2) == 1), error_message = "Make sure that your answer is a character!")
+  TestCase((q_2 %in% c("a", "b", "c", "d")), error_message = "Make sure that your answer is one of the options listed!")
+  TestCase((q_2 == "b"), error_message = "Incorrect choice, try again!")
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem3 = function() {
+  problemNumber <<- 3
+  scores[problemNumber] <<- 0
+  num_tests <<- 3
+  tests_failed <<- num_tests
+  
+  TestCase((class(q_3) == "character" & length(q_3) == 1), error_message = "Make sure that your answer is a character!")
+  TestCase((q_3 %in% c("a", "b", "c", "d")), error_message = "Make sure that your answer is one of the options listed!")
+  TestCase((q_3 == "c"), error_message = "Incorrect choice, try again!")
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem4 = function() {
+  problemNumber <<- 4
+  scores[problemNumber] <<- 0
+  num_tests <<- 5
+  tests_failed <<- num_tests
+  q_4_correct = readRDS("setup/autograder-setup/hw1_r_quiz/q_4_correct.RDS")
+  
+  TestCase((length(q_4) == 4), error_message = "Make sure your vector contains 4 items")
+  TestCase((q_4[1] == q_4_correct[1]), error_message = "Check the first item of your vector")
+  TestCase((q_4[2] == q_4_correct[2]), error_message = "Check the second item of your vector")
+  TestCase((q_4[3] == q_4_correct[3]), error_message = "Check the third item of your vector")
+  TestCase((q_4[4] == q_4_correct[4]), error_message = "Check the fourth item of your vector")
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem5 = function() {
+  problemNumber <<- 5
+  scores[problemNumber] <<- 0
+  num_tests <<- 4
+  tests_failed <<- num_tests
+  q_5_correct = readRDS("setup/autograder-setup/hw1_r_quiz/q_5_correct.RDS")
+  
+  TestCase((length(q_5) == 3), error_message = "Make sure your vector contains 3 items")
+  TestCase((q_5[1] == q_5_correct[1]), error_message = "Check the first item of your vector")
+  TestCase((q_5[2] == q_5_correct[2]), error_message = "Check the second item of your vector")
+  TestCase((q_5[3] == q_5_correct[3]), error_message = "Check the third item of your vector")
+
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem6 = function() {
+  problemNumber <<- 6
+  scores[problemNumber] <<- 0
+  num_tests <<- 3
+  tests_failed <<- num_tests
+
+  TestCase((length(q_6)==2), "Make sure that q_6 contains two column names")
+  TestCase((q_6[1] == colnames(df)[1]), "Check the first column name, make sure you're not manually typing it in")
+  TestCase((q_6[2] == colnames(df)[2]), "Check the second column name, make sure you're not manually typing it in")
+  
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem7 = function() {
+  problemNumber <<- 7
+  scores[problemNumber] <<- 0
+  num_tests <<- 3
+  tests_failed <<- num_tests
+  q_7_correct = readRDS("setup/autograder-setup/hw1_r_quiz/q_7_correct.RDS")
+  
+  TestCase((nrow(q_7_correct)==nrow(q_7)), "Make sure that q_6 contains the same number of rows as the original table")
+  TestCase((q_7$total_cost[1] == q_7_correct$total_cost[1]), "Check your calculations, make sure that you're multiplying each amount_remaining by the right price_per_item")
+  TestCase((sum(q_7$total_cost) == sum(q_7_correct$total_cost)), "Check your calculations, make sure that you're multiplying each amount_remaining by the right price_per_item")
+  
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem8 = function() {
+  problemNumber <<- 8
+  scores[problemNumber] <<- 0
+  num_tests <<- 3
+  tests_failed <<- num_tests
+  
+  TestCase((class(q_8) == "character" & length(q_8) == 1), error_message = "Make sure that your answer is a character!")
+  TestCase((q_8 %in% c("a", "b", "c", "d")), error_message = "Make sure that your answer is one of the options listed!")
+  TestCase((q_8 == "a"), error_message = "Incorrect choice, try again!")
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem9 = function() {
+  problemNumber <<- 9
+  scores[problemNumber] <<- 0
+  num_tests <<- 4
+  tests_failed <<- num_tests
+
+  TestCase((nrow(q_9) != 0), "Your table is empty. Check the conditions that you're filtering by")
+  TestCase((unique(q_9$major) == "Public Health"), "Make sure to filter the student_info table down to only Public Health majors")
+  TestCase((ncol(q_9) == 4), "Make sure to keep all of the columns from the original student_info table")
+  TestCase(("Alice" %in% q_9$name & !("Bob" %in% q_9$name)), "Make sure to keep students matched to their correct majors after filtering the table.")
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+CheckProblem10 = function() {
+  problemNumber <<- 10
+  scores[problemNumber] <<- 0
+  num_tests <<- 3
+  tests_failed <<- num_tests
+  age_order = readRDS("setup/autograder-setup/hw1_r_quiz/q_10_correct.RDS")
+  
+  TestCase((nrow(q_10) == 5), "Make sure to keep all of the rows from the original student_info table")
+  TestCase((ncol(q_10) == 4), "Make sure to keep all of the columns from the original student_info table")
+  TestCase((all((q_10$age - age_order) == 0)), "Make sure you sort by descending order")
+
+  
+  print("Correct!")
+  ReturnScore(problemNumber, num_tests, tests_failed)
+  scores[problemNumber] <<- 1
+}
+
+
+
